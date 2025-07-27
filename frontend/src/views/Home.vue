4354ce36bc3c472d3f2522c4c15201d0fe41c38f
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-28 04:55:05 +0530

    set home route with hello message

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <h1>Hello!</h1>
+</template>
+
+<style lang="scss">
+h1 {
+  text-align: center;
+  font-size: 5rem;
+  background: rgb(61, 187, 187);
+}
+</style>
