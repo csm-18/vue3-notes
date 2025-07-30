91ef329e0c6c371bf9609d4fba15cb84ab109f51
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-07-30 12:09:32 +0530

    complete steps

--- frontend/src/components/ProjectSetup.vue
+++ frontend/src/components/ProjectSetup.vue
@@ -37,6 +37,134 @@ import CodeBlock from './CodeBlock.vue'
       <p>4. Install and test run</p>
       <code-block class="code" dollar="true" :code="['bun install']" />
       <code-block class="code" dollar="true" :code="['bun dev']" />
+      <p>5. Add sass</p>
+      <code-block class="code" dollar="true" :code="['bun add -D sass-embedded']" />
+      <h3>Refactoring for Hello app</h3>
+      <p>6. Refactor <b>/index.html</b></p>
+      <ul>
+        <li>remove icon or change source</li>
+        <li>change title of the page</li>
+      </ul>
+      <code-block
+        class="code"
+        :code="[
+          '<!DOCTYPE html>',
+          '<html lang=\'en\'>',
+          '<head>',
+          '  <meta charset=\'utf-8\'>',
+          '  <meta name=\'viewport\' content=\'width=device-width, initial-scale=1.0\'>',
+          '  <title>Hello App</title>',
+          '</head>',
+          '<body>',
+          '  <div id=\'app\'></div>',
+          '  <script type=\'module\' src=\'/src/main.js\'></script>',
+          '</body>',
+          '</html>',
+        ]"
+      />
+      <p>7. Add routes and guard to <b>src/router/index.js</b></p>
+      <code-block
+        class="code"
+        :code="[
+          'import { createRouter, createWebHistory } from \'vue-router\'',
+          'import Home from \'@/views/Home.vue\'',
+          '',
+          'const router = createRouter({',
+          '  history: createWebHistory(import.meta.env.BASE_URL),',
+          '  routes: [',
+          '    {',
+          '      path: \'/\',',
+          '      name: \'home\',',
+          '      component: Home,',
+          '      meta: { title: \'Home\' }, //Add title here',
+          '    },',
+          '  ],',
+          '})',
+          '',
+          'router.beforeEach((to, from, next) => {',
+          '  const defaultTitle = \'Hello\' //Fallback title',
+          '  document.title = String(to.meta.title) || defaultTitle',
+          '  next()',
+          '})',
+          '',
+          'export default router',
+        ]"
+      />
+      <p>8. Add global styling</p>
+      <ul>
+        <li>create src/assets folder</li>
+        <li>create src/assets/main.css</li>
+      </ul>
+      <code-block
+        class="code"
+        :code="[
+          'html,',
+          'body {',
+          '  margin: 0;',
+          '  padding: 0;',
+          '}',
+          '',
+          ':root {',
+          ' width: 100vw;',
+          ' height: 100vh;',
+          '}',
+        ]"
+      />
+      <p>Add <b>main.css</b> to <b>src/main.js</b></p>
+      <code-block
+        class="code"
+        :code="[
+          'import \'./assets/main.css\' //add this line',
+          '',
+          'import { createApp } from \'vue\'',
+          'imort { createPinia } from \'pinia\'',
+          '',
+          'import App from \'./App.vue\'',
+          'import router from \'./router\'',
+          '',
+          'const app = createApp(App)',
+          '',
+          'app.use(createPinia())',
+          'app.use(router)',
+          '',
+          'app.mount(\'#app\')',
+        ]"
+      />
+      <p>9. Create <b>src/components</b> folder (for storing components).</p>
+      <p>10. Create <b>src/views</b> folder (for storing page components).</p>
+      <p>11. Create <b>src/views/Home.vue</b></p>
+      <code-block
+        class="code"
+        :code="[
+          '<script setup></script>',
+          '',
+          '<template>',
+          '  <h1>Hello</h1>',
+          '</template>',
+          '',
+          '<style scoped lang=\'scss\'>',
+          'h1 {',
+          '  text-align: center;',
+          '  font-size: 5rem;',
+          '}',
+          '</style>',
+        ]"
+      />
+      <p>12. Change <b>src/App.vue</b></p>
+      <code-block
+        class="code"
+        :code="[
+          '<script setup>',
+          'import {RouterView} from \'vue-router\'',
+          '</script>',
+          '',
+          '<template>',
+          '  <RouterView />',
+          '</template>',
+          '',
+          '<style scoped lang=\'scss\'><style>',
+        ]"
+      />
     </div>
   </div>
 </template>
@@ -81,6 +209,10 @@ import CodeBlock from './CodeBlock.vue'
 
     .code {
       margin-left: 2rem;
+    }
+
+    h3 {
+      margin-top: 5rem;
     }
   }
 }
